9e7e0c987c6a240a2578affe910c9f19117203dc
Venus Lumanglas <vlumanglas30@gmail.com>
2021-06-07 13:21:52 +0800

    Add form field validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article = Article.find(params[:id])
-    # @article.destroy
+    @article.destroy
 
     redirect_to root_path
   end
